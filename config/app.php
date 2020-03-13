b03fece872d7b51b106805ed17145e6e6879fea7
xhoanaqesja <xhoana2009@live.com>
2020-03-13 13:28:06 +0100

    added DebugBar for laravel, Run Composer Install again

--- config/app.php
+++ config/app.php
@@ -161,20 +161,25 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        
         /*
          * Package Service Providers...
          */
-
+        
         /*
-         * Application Service Providers...
-         */
+        * Application Service Providers...
+        */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        /*
+         * Debugger Plugin...
+         */
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
@@ -227,6 +232,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
